899240d765248853259daffc951ce58823346c4b
Tyler Hunt <tyler@tylerhunt.com>
2026-04-29 08:14:49 -0400

    Split `Registry` into its own file
    Move `Bates.Middleware.Registry` out of `middleware.ex` and into
    `source/lib/bates/middleware/registry.ex` to follow the one-module-per-file
    convention. `middleware.ex` now contains only the `Bates.Middleware`
    behaviour and `apply_pipeline/3`.

---
+++ source/lib/bates/middleware/registry.ex
@@ -0,0 +1,25 @@
+defmodule Bates.Middleware.Registry do
+  @builtins %{
+    "asdf" => Bates.Middleware.Asdf,
+    "port" => Bates.Middleware.Port
+  }
+
+  def lookup(name) when is_binary(name) do
+    case Map.fetch(all(), name) do
+      {:ok, module} -> {:ok, module}
+      :error -> {:error, :unknown}
+    end
+  end
+
+  def lookup!(name) when is_binary(name) do
+    case lookup(name) do
+      {:ok, module} -> module
+      {:error, :unknown} -> raise "Unknown middleware: #{inspect(name)}"
+    end
+  end
+
+  defp all do
+    extra = Application.get_env(:bates, :extra_middleware, %{})
+    Map.merge(@builtins, extra)
+  end
+end
